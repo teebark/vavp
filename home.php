f56ff0305c3a77db65a581a4c1a47b0fcb83b87d
teebark <terry@teebark.com>
2018-09-03 10:00:01 -0400

    baseline

---
+++ home.php
@@ -0,0 +1,96 @@
+<?php
+/* Modified version of Divi index.php for vavp blog page */
+get_header(); 
+/* echo do_shortcode('[et_pb_section global_module="189"][/et_pb_section]'); */ ?>
+
+<div id="main-content">
+	<div class="container">
+		<div id="content-area" class="clearfix">
+			<div id="left-area">
+		<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post(); ?>
+					<article id="post-<?php the_ID(); ?>" 
+					<?php post_class( 'et_pb_post' ); ?>>
+
+				<?php
+					$thumb = '';
+
+					$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
+
+					$height = (int) apply_filters( 'et_pb_index_blog_image_height', 675 );
+					$classtext = 'et_pb_post_main_image';
+					$titletext = get_the_title();
+					$thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Blogimage' );
+					$thumb = $thumbnail["thumb"];
+
+					et_divi_post_format_content();
+					 
+					$post_format = et_pb_post_format();
+
+					if ( ! in_array( $post_format, array( 'link', 'audio', 'quote' ) ) ) {
+						if ( 'video' === $post_format && false !== ( $first_video = et_get_first_video() ) ) :
+							printf(
+								'<div class="et_main_video_container">
+									%1$s
+								</div>',
+								$first_video
+							);
+						elseif ( ! in_array( $post_format, array( 'gallery' ) ) && 'on' === et_get_option( 'divi_thumbnails_index', 'on' ) && '' !== $thumb ) : ?>
+							<a href="<?php the_permalink(); ?>">
+								<?php print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height ); ?>
+							</a>
+					<?php
+						elseif ( 'gallery' === $post_format ) :
+							et_pb_gallery_images();
+						endif;
+					} ?>
+
+				<?php if ( ! in_array( $post_format, array( 'link', 'audio', 'quote' ) ) ) : ?>
+					<?php if ( ! in_array( $post_format, array( 'link', 'audio' ) ) ) : ?>
+						<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<?php endif; ?>
+
+					<?php
+						et_divi_post_meta();
+
+						if ( 'on' !== et_get_option( 'divi_blog_style', 'false' ) || ( is_search() && ( 'on' === get_post_meta( get_the_ID(), '_et_pb_use_builder', true ) ) ) ) {
+							truncate_post( 270 );
+							$val = get_post_meta(get_the_ID(), 'spanish_title', true); ?>
+							<h2 class="spanish-title"><a href="../wp-content/themes/virginia-avp/spanish-version.php"><?php echo $val; ?></a></h2>	
+							<?php
+							$val = get_post_meta(get_the_ID(), 'spanish_content', true);    
+							$val = strip_shortcodes( $val );    
+							$val = apply_filters('the_content', $val);	    
+							$val = str_replace(']]&gt;', ']]&gt;', $val);	    
+							$excerpt_length = 20; // 20 words	    
+							$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');	  
+							$val = wp_trim_words( $val, $excerpt_length, $excerpt_more ); ?>  
+							<p class="spanish-meta"><?php echo $val; ?></p>   
+							<?php
+						} else {
+							the_content();
+						}
+					?>
+				<?php endif; ?>
+
+					</article> <!-- .et_pb_post -->
+			<?php
+					endwhile;
+
+					if ( function_exists( 'wp_pagenavi' ) )
+						wp_pagenavi();
+					else
+						get_template_part( 'includes/navigation', 'index' );
+				else :
+					get_template_part( 'includes/no-results', 'index' );
+				endif;
+			?>
+			</div> <!-- #left-area -->
+
+			<?php get_sidebar(); ?>
+		</div> <!-- #content-area -->
+	</div> <!-- .container -->
+</div> <!-- #main-content -->
+
+<?php get_footer(); ?>
